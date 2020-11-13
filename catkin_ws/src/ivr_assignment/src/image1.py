6e12b978ea50e217163937d930ccbefbe1730552
orgesskura <orgesskura@github.com>
2020-11-13 23:29:38 +0000

    added translation matrices

--- catkin_ws/src/ivr_assignment/src/image1.py
+++ catkin_ws/src/ivr_assignment/src/image1.py
@@ -10,6 +10,8 @@ from sensor_msgs.msg import Image
 from std_msgs.msg import Float64MultiArray, Float64
 from cv_bridge import CvBridge, CvBridgeError
 import message_filters
+import sympy
+from scipy.optimize import least_squares
 
 class image_converter:
 
@@ -35,7 +37,9 @@ class image_converter:
     self.joint2_pub = rospy.Publisher("/robot/joint2_position_controller/command", Float64, queue_size=10)
     self.joint3_pub = rospy.Publisher("/robot/joint3_position_controller/command", Float64, queue_size=10)
     self.joint4_pub = rospy.Publisher("/robot/joint4_position_controller/command", Float64, queue_size=10)
-
+    
+    #save current angles so we can use least squares
+    self.current_angles = [0,0,0]
 
 
     # record the begining time
@@ -140,19 +144,25 @@ class image_converter:
         z_cord = self.detect_pos_z(self.cv_image1,color_sphere)
         return [x_cord,y_cord,z_cord]
   
-  #calculate joint angles by using both images(for ex if i am in x-z plane rotating around x or z does not change anything)
-  def calcJoint_angles(self):
-        a = self.pixel2meter(self.cv_image1)
-        b = self.pixel2meter(self.cv_image2)
-        centerBlue1 = a * self.detect_blue(self.cv_image1)
-        centerGreen1 = a * self.detect_green(self.cv_image1)
-        centerBlue2 = b * self.detect_blue(self.cv_image2)
-        centerGreen2 = b * self.detect_green(self.cv_image2)
-        centerRed1 = a * self.detect_blue(self.cv_image1)
-        ja1 = np.arctan2(centerBlue1[0]- centerGreen1[0], centerBlue1[1] - centerGreen1[1])
-        ja2 = np.arctan2(centerBlue2[0]- centerGreen2[0], centerBlue2[1] - centerGreen2[1])
-        ja3 = np.arctan2(centerGreen1[0] - centerRed1[0],centerGreen1[1] - centerRed1[1]) - ja1
-        return [ja1,ja2,ja3]
+  #calculate joint angles by using least squares method
+  def calcJoint_angles(self,thetas):
+        #calculate translation matrices
+       trans23 = np.array([[1,0,0,0],
+       [0,np.cos(thetas[0]),-np.sin(thetas(0)),0],
+       [0,np.sin(thetas[0]),np.cos(thetas[0]),0],
+       [0,0,0,1]])
+       trans12 = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,2.5],[0,0,0,1]])
+       trans34 = np.array([[np.cos(thetas[1]),0,np.sin(thetas[1]),0],
+       [0,1,0,0]
+       [-np.sin(thetas[1]),0,np.cos(thetas[1]),3.5]
+       [0,0,0,1]])
+       trans45 = np.array([[1,0,0,0],
+       [0,np.cos(thetas[2]),-np.sin(thetas[2]),0]
+       [0,np.sin(thetas[2]),np.cos(thetas[2],3)]
+       [0,0,0,1]])
+
+
+
         
 
 
@@ -186,26 +196,13 @@ class image_converter:
     cv2.waitKey(1)
     # Publish the results
     try: 
-      self.image_pub1.publish(self.bridge.cv2_to_imgmsg(self.cv_image2, "bgr8"))
+      self.image_pub1.publish(self.bridge.cv2_to_imgmsg(self.cv_image1, "bgr8"))
       self.joint2_pub.publish(joint2_val)
       self.joint3_pub.publish(joint3_val)
       self.joint4_pub.publish(joint4_val)
       self.joints_pub.publish(self.joints)
     except CvBridgeError as e:
       print(e)
-
-
-
-    # Recieve data from camera 2, process it to use it
-  def callback2(self,data):
-    # Recieve the image
-    try:
-      self.cv_image2 = self.bridge.imgmsg_to_cv2(data, "bgr8")
-    except CvBridgeError as e:
-      print(e)
-    # Uncomment if you want to save the image
-    #cv2.imwrite('image_copy.png', cv_image)
-
     
 
 
